7aace914cb563d2cc50acf3c62200a9919613573
PrashantChaudhary-rgb <73767749+PrashantChaudhary-rgb@users.noreply.github.com>
2021-08-13 22:18:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ connectDB();
 app.use(express.static('public'));
 app.use(express.json());
 
-// const cors=require('cors');
-// const corsOptions={
-//     origin:process.env.ALLOWED_CLIENTS
-// }
-// app.use(cors(corsOptions));
+const cors=require('cors');
+const corsOptions={
+    origin:process.env.ALLOWED_CLIENTS.split(',')
+}
+app.use(cors(corsOptions));
 
 
 
