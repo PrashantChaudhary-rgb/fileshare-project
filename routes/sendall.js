210927d6e095c688e20f385f5ef8d269b4358543
PrashantChaudhary-rgb <73767749+PrashantChaudhary-rgb@users.noreply.github.com>
2021-08-14 10:49:20 +0530

    Update sendall.js

--- routes/sendall.js
+++ routes/sendall.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 const File = require('../models/file');
 
 router.get('/', async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
     const file = await File.find();
         console.log(file);
         return res.send(file);
